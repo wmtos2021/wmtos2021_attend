ddeab9c78e80ccfda232802ed6552dec561b859a
wmtos2021 <wmtos2021@gmail.com>
2026-08-20 20:08:32 +0900

    Ver 1.0.1(2)

--- attend/attendPop.js
+++ attend/attendPop.js
@@ -36,20 +36,18 @@ export function showAttendPopup(
     message,
     callback
 ) {
-    setTimeout(() => {
-        resultAttendImage.src = image;
-        attendPoint.textContent = point;
-        wisdomTitle.textContent = title;
-        wisdomMessage.textContent = message;
-        resultAttendImage.style.display = "block";
-        attendPoint.parentElement.style.display = "block";
-        attendConfirmBtn.style.display = "block";
-        wisdomTitle.style.display = "block";
-        wisdomMessage.style.marginBottom = "0px";
-        wisdomMessage.style.removeProperty("font-size");
-        confirmCallback = callback;
-        attendModal.classList.remove("hidden");
-    }, 500);
+    resultAttendImage.src = image;
+    attendPoint.textContent = point;
+    wisdomTitle.textContent = title;
+    wisdomMessage.textContent = message;
+    resultAttendImage.style.display = "block";
+    attendPoint.parentElement.style.display = "block";
+    attendConfirmBtn.style.display = "block";
+    wisdomTitle.style.display = "block";
+    wisdomMessage.style.marginBottom = "0px";
+    wisdomMessage.style.removeProperty("font-size");
+    confirmCallback = callback;
+    attendModal.classList.remove("hidden");
 }
 
 
